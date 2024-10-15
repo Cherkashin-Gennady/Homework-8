80956669b1c904f2a9bd1c5a0dd03e4797c3e60e
Геннадий <cherri.irk@yandex.ru>
2024-10-16 01:51:24 +0800

    Home 8

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Task 1
         System.out.println("Задача 1");
-        int[] arr = new int[3];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
+        int[] arr = new int[]{1, 2, 3};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
