e6d2c97b2ac1bff0d21897ab317a681d740623b5
Геннадий <cherri.irk@yandex.ru>
2024-10-16 01:39:13 +0800

    Hw 8

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Task 1
+        System.out.println("Задача 1");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        double[] decimalNum = {1.57, 7.654, 9.986};
+        for (int n = 0; n < arr.length; n++) {
+            System.out.println(decimalNum[n]);
+        }
+        byte[] chars = {3, 7, 15, 2};
+        for (int v = 0; v < chars.length; v++) {
+            System.out.println(chars[v]);
+        }
+        // Task 2
+        System.out.println("Задача 2");
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(decimalNum));
+        System.out.println(Arrays.toString(chars));
+        // Task 3
+        System.out.println("Задача 3");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(arr[i]);
+                break;
+            }
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println();
+        for (int n = arr.length - 1; n >= 0; n--) {
+            if (n == 0) {
+                System.out.println(decimalNum[n]);
+                break;
+            }
+            System.out.print(decimalNum[n] + ", ");
+        }
+        for (int v = chars.length - 1; v >= 0; v--) {
+            if (v == 0) {
+                System.out.println(chars[v]);
+                break;
+            }
+            System.out.print(chars[v] + ", ");
+        }
+        // Task 4
+        System.out.println("Задача 4");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i] = arr[i] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+
