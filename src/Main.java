d73338f02ba05e0fd74ff1bc14ae0830cc142ccc
Геннадий <cherri.irk@yandex.ru>
2024-10-16 02:27:00 +0800

    corrections in the code

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,20 @@ public class Main {
         }
         // Task 2
         System.out.println("Задача 2");
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(decimalNum));
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length-1) {
+                System.out.println(arr[i]);
+                break;
+            }
+            System.out.print(arr[i] + ", ");
+        }
+        for (int n = 0; n < arr.length; n++) {
+            if (n == decimalNum.length-1) {
+                System.out.println(decimalNum[n]);
+                break;
+            }
+            System.out.print(decimalNum[n] + ", ");
+        }
         System.out.println(Arrays.toString(chars));
         // Task 3
         System.out.println("Задача 3");
